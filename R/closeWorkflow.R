ffb66ff24fbf92171f9c2ffa828f29b88457987d
eblondel <emmanuel.blondel1@gmail.com>
2018-09-25 13:07:29 +0200

    fix closing stuff

--- R/closeWorkflow.R
+++ R/closeWorkflow.R
@@ -16,31 +16,31 @@ closeWorkflow <- function(config){
     config$logger.info("Closing database connection")
     dbDisconnect(config$sdi$db)
     config$sdi$db <- config$sdi$db_config
-    rm(config$sdi$db_config)
+    config$sdi$db_config <- NULL
   }
   #Geoserver API manager
   if(!is.null(config$sdi$geoserver)){
     config$logger.info("Reset Geoserver API manager")
     config$sdi$geoserver <- config$sdi$geoserver_config
-    rm(config$sdi$geoserver_config)
+    config$sdi$geoserver_config <- NULL
   }
   #Geonetwork API manager
   if(!is.null(config$sdi$geonetwork)){
     config$logger.info("Reset Geonetwork API manager")
     config$sdi$geonetwork <- config$sdi$geonetwork_config
-    rm(config$sdi$geonetwork_config)
+    config$sdi$geonetwork_config <- NULL
   }
   #WFS
   if(!is.null(config$sdi$wfs)){
     config$logger.info("Reset WFS client")
     config$sdi$wfs <- config$sdi$wfs_config
-    rm(config$sdi$wfs_config)
+    config$sdi$wfs_config <- NULL
   }
   #CSW
   if(!is.null(config$sdi$csw)){
     config$logger.info("Reset CSW client")
     config$sdi$csw <- config$sdi$csw_config
-    rm(config$sdi$csw_config)
+    config$sdi$csw_config <- NULL
   }
   setwd(config$wd)
 }
