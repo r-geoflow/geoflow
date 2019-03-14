725e8d63296197cb4d8ea4dd8c22882e62bffee5
eblondel <emmanuel.blondel1@gmail.com>
2019-03-14 11:47:51 +0100

    add ISO 19115 to xml naming

--- R/geoflow_action_geometa_create_iso_19115.R
+++ R/geoflow_action_geometa_create_iso_19115.R
@@ -249,7 +249,7 @@ geometa_create_iso_19115 <- function(entity, config, options){
   
   #we save the metadata
   saveRDS(md, file.path(getwd(), "metadata", paste0(entity$identifiers[["id"]], ".rds")))
-  md$save(file.path(getwd(), "metadata", paste0(entity$identifiers[["id"]], ".xml")), inspire = inspire)
+  md$save(file.path(getwd(), "metadata", paste0(entity$identifiers[["id"]], "_ISO-19115.xml")), inspire = inspire)
   
   return(md)
 }
