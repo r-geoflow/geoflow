a74d3e15054587d59719df47afafd586f0fdea15
eblondel <emmanuel.blondel1@gmail.com>
2019-03-21 16:36:04 +0100

    more config tests

--- tests/testthat/test_initWorkflow.R
+++ tests/testthat/test_initWorkflow.R
@@ -24,6 +24,18 @@ test_that("gsheet handler is working properly",{
   expect_equal(length(CFG$metadata$content$entities), 1L)
 })
 
+test_that("gsheet handler - entities only",{
+  #try to init workflow
+  cfg_file = system.file("extdata/config_example_gsheets_entities_only.json", package = "geoflow")
+  CFG <- initWorkflow(cfg_file)
+  #check
+  expect_is(CFG$metadata$content, "list")
+  expect_equal(length(CFG$metadata$content), 2L)
+  expect_equal(names(CFG$metadat$content), c("contacts", "entities"))
+  expect_equal(length(CFG$metadata$content$contacts), 3L)
+  expect_equal(length(CFG$metadata$content$entities), 1L)
+})
+
 test_that("ISO 19115 metadata produced out of gsheets",{
   #try to init workflow
   cfg_file = system.file("extdata/config_example_gsheets_action_geometa.json", package = "geoflow")
