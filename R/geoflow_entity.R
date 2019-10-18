abc49ed61f0c52c52b1577215954ced31e53e097
eblondel <emmanuel.blondel1@gmail.com>
2019-10-18 09:34:00 +0200

    fix crs comparison

--- R/geoflow_entity.R
+++ R/geoflow_entity.R
@@ -314,8 +314,8 @@ geoflow_entity <- R6Class("geoflow_entity",
                  sf.crs <- sf::st_crs(sf.data)
                  if(!is.na(sf.crs)){
                    srid <- if(!is.null(self$srid)) self$srid else ""
-                   if(srid != sf.crs){
-                    config$logger.info("Overwriting entity srid [%s] with shapefile srid [%s]") 
+                   if(srid != sf.crs$epsg){
+                    config$logger.info(sprintf("Overwriting entity srid [%s] with shapefile srid [%s]", srid, sf.crs$epsg)) 
                     self$setSrid(sf.crs$epsg)
                    }
                  }
