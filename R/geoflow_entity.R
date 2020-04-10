daa0e65aa15a9bda097d743e8ea14ec2fb9aebb2
eblondel <emmanuel.blondel1@gmail.com>
2020-04-10 17:41:05 +0200

    fix asDataFrame

--- R/geoflow_entity.R
+++ R/geoflow_entity.R
@@ -1026,7 +1026,9 @@ geoflow_entity <- R6Class("geoflow_entity",
             }
             outdata <- paste0(outdata, "uploadSource:", out_upload_sources, line_separator)
           }
-          outdata <- paste0(outdata, "uploadType:", tolower(as.character(self$data$uploadType)), line_separator)
+          if(!is.null(self$data$uploadType)){
+            outdata <- paste0(outdata, "uploadType:", tolower(as.character(self$data$uploadType)), line_separator)
+          }
           outdata <- paste0(outdata, "upload:", tolower(as.character(self$data$upload)), line_separator)
           
           if(!is.null(self$data$featureType)){
