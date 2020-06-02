a9f34f84eb5ff56cf7ffc6e60e58a447765306ba
abennici <alexandre-ben@hotmail.fr>
2020-06-02 14:31:21 +0200

    Add a function to reformat in shp and dbi and export data from various formats

---
+++ R/writeWorkflowJobDataResource.R
@@ -0,0 +1,37 @@
+#' @name writeWorkflowJobDataResource
+#' @aliases writeWorkflowJobDataResource
+#' @title writeWorkflowJobDataResource
+#' @description \code{writeWorkflowJobDataResource} allows to transform datasource into different formats
+#'
+#' @usage writeWorkflowJobDataResource(obj,type)
+#'
+#' @param obj a sf file
+#' @param type format to convert into list :"shp","dbtable 
+#' 
+#' @author Alexandre Bennici, \email{bennicialexandre@@gmail.com}
+#' @export
+#'
+
+writeWorkflowJobDataResource <- function(entity, config,obj,resourcename,type){
+  if(!type%in%c("shp","dbtable")) config$logger.error('type must be in : "shp" , "dbtable"')
+if(is.null(obj))obj=entity$data$features
+  resourcename_parts <- unlist(strsplit(resourcename, "\\."))
+  if(length(resourcename_parts)>1) resourcename <- resourcename_parts[1]
+  switch(type,
+         "shp"={
+           config$logger.info(sprintf("Format type: %s", type))
+           st_write(obj = obj, paste0("./data/",resourcename,".shp"), delete_layer = TRUE)
+           config$logger.info("write shp file to data job directory")
+           zip::zipr(zipfile = paste0("./data/",resourcename, ".zip"), files = paste0(getwd(),"./data/",list.files(path="./data",pattern = resourcename)))
+           config$logger.info("zip datafiles for server export")
+           df<-st_read(paste0("./data/",resourcename,".shp"), quiet=TRUE)
+           entity$data$features<-df
+           },
+          "dbtable"={
+           config$logger.info(sprintf("Format type: %s", type))
+           st_write(obj = obj, dsn = config$software$output$dbi, layer =resourcename , layer_options = 'OVERWRITE=YES')
+           entity$data$features<-obj
+           }
+        )
+}
+
