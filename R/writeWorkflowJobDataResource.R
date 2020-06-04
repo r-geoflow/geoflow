873e3ab55e0f048fb3605d7947576e14a76028e4
abennici <alexandre-ben@hotmail.fr>
2020-06-04 09:40:29 +0200

    up writeWorkflowJobDataResource

--- R/writeWorkflowJobDataResource.R
+++ R/writeWorkflowJobDataResource.R
@@ -3,23 +3,53 @@
 #' @title writeWorkflowJobDataResource
 #' @description \code{writeWorkflowJobDataResource} allows to transform datasource into different formats
 #'
-#' @usage writeWorkflowJobDataResource(obj,type)
+#' @usage writeWorkflowJobDataResource(entity,config,obj,useFeatures,resourcename,useUploadSource,type)
 #'
 #' @param entity a entity object as read by \code{geoflow_entity} 
 #' @param config a configuration object as read by \code{initWorkflow}
 #' @param obj a sf file
+#' @param use Features a boolean condition to define if features must be attach to obj file
 #' @param resourcename name of data input
+#' @param useUploadSource a boolean condition to define if resourcename is same as uploadSource information
 #' @param type format to convert into list :"shp","dbtable 
 #' 
 #' @author Alexandre Bennici, \email{bennicialexandre@@gmail.com}
 #' @export
 #'
 
-writeWorkflowJobDataResource <- function(entity, config,obj=NULL,resourcename,type){
-  if(!type%in%c("shp","dbtable")) config$logger.error('type must be in : "shp" , "dbtable"')
-if(is.null(obj))obj=entity$data$features
+writeWorkflowJobDataResource <- function(entity, config,obj=NULL,useFeatures=FALSE,resourcename,useUploadSource=FALSE,type){
+  config$logger.info("------------------------------------------------------------------------------")
+  config$logger.info("Write data resource start")
+  if(is.null(obj) && !useFeatures){
+    errMsg<-"Error: specify at least an object or useFeatures = TRUE"
+    config$logger.error(errMsg)
+    stop(errMsg)
+  }
+  if(useFeatures){
+    config$logger.info("No object specified, use entity$data$feature by default")
+    obj<-entity$data$features
+  }
+  
+  if(is.null(resourcename) && useUploadSource){
+    errMsg<-"Error: specify a resourcename or useUploadSource = TRUE"
+    config$logger.error(errMsg)
+    stop(errMsg)  
+  }
+  
+  if(useUploadSource){
+    config$logger.info("No resourcename specified, use entity$data$uploadSource by default")
+    resourcename<-entity$data$uploadSource[[1]]
+  }
+  
+  if(!type %in% c("shp","dbtable")){
+    errMsg<-"Error: unrecognized type, specify a type at this list : 'shp','dbtable'"
+    config$logger.error(errMsg)
+    stop(errMsg)
+  } 
+  
   resourcename_parts <- unlist(strsplit(resourcename, "\\."))
   if(length(resourcename_parts)>1) resourcename <- resourcename_parts[1]
+
   switch(type,
          "shp"={
            config$logger.info(sprintf("Format type: %s", type))
@@ -27,11 +57,23 @@ if(is.null(obj))obj=entity$data$features
            config$logger.info("write shp file to data job directory")
            zip::zipr(zipfile = paste0("./data/",resourcename, ".zip"), files = paste0(getwd(),"./data/",list.files(path="./data",pattern = resourcename)))
            config$logger.info("zip datafiles for server export")
+           if(useFeatures){
+           config$logger.info("object use to data features") 
+           df<-st_read(paste0("./data/",substring(resourcename,1,nchar(resourcename)-4),".shp"), quiet=TRUE) 
+           entity$data$features<-df
+           }
            },
           "dbtable"={
+           if(is.null(config$software$output$dbi)){
+             errMsg<-"Error: no dbi output detected, branch one in the config"
+             config$logger.error(errMsg)
+             stop(errMsg)   
+           }  
            config$logger.info(sprintf("Format type: %s", type))
            st_write(obj = obj, dsn = config$software$output$dbi, layer =resourcename , layer_options = 'OVERWRITE=YES')
            }
         )
+config$logger.info("Write data resource end")
+config$logger.info("------------------------------------------------------------------------------")
 }
 
