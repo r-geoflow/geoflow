f7674de881f3e3f59cef804d3f7a155a90e1a827
Bennici <alexandre-ben@hotmail.fr>
2020-07-07 10:30:20 +0200

    add ifelse condition for option creatIndexes

--- R/geoflow_action_writeWorkflowJobDataResource.R
+++ R/geoflow_action_writeWorkflowJobDataResource.R
@@ -1,6 +1,8 @@
 #For import in dbi
 action_import_dbi<-function(entity,config,options){
-  writeWorkflowDataResource(entity=entity,config=config,obj=NULL,useFeatures=TRUE,resourcename=NULL,useUploadSource=TRUE,createIndexes=options$createIndexes,type="dbtable")}
+  writeWorkflowDataResource(entity=entity,config=config,obj=NULL,useFeatures=TRUE,resourcename=NULL,useUploadSource=TRUE,createIndexes=options$createIndexes,type="dbtable")
+  #options
+  createIndexes<-ifelse(!is.null(options$createIndexes), options$createIndexes, FALSE) }
 
 #For enrich features of shp
 action_import_shp<-function(entity,config,options){
@@ -8,6 +10,7 @@ action_import_shp<-function(entity,config,options){
 
 #For generic action
 action_import_generic<-function(entity,config,options){
-  writeWorkflowDataResource(entity=entity,config=config,obj=NULL,useFeatures=TRUE,resourcename=NULL,useUploadSource=TRUE,createIndexes=options$createIndexes,type=options$type)}
-
+  writeWorkflowDataResource(entity=entity,config=config,obj=NULL,useFeatures=TRUE,resourcename=NULL,useUploadSource=TRUE,createIndexes=options$createIndexes,type=options$type)
+  #options
+  createIndexes<-ifelse(!is.null(options$createIndexes), options$createIndexes, FALSE) }
     
