f3122d1fb5efe7c8f13de5ba61103d36043ef0ad
eblondel <emmanuel.blondel1@gmail.com>
2020-10-14 11:16:04 +0200

    #75 eml action - rights, dataTable/spatialVector

--- R/geoflow_action_eml_create_eml.R
+++ R/geoflow_action_eml_create_eml.R
@@ -112,6 +112,9 @@ eml_create_eml <- function(entity, config, options){
   if(!is.null(entity$descriptions$info)) dataset$additionalInfo <- list(para = entity$descriptions$info)
   
   #TODO intellectualRights
+  rights <- entity$rights[sapply(entity$rights, function(x){x$key == "use"})]
+  dataset$intellectualRights <- paste(sapply(rights, function(x){x$value}), sep = "\n")
+  
   #TODO license
   #TODO distribution
   
@@ -188,8 +191,200 @@ eml_create_eml <- function(entity, config, options){
   if(!is.null(config$profile$project)) project <- config$profile$project
   if(!is.null(entity$descriptions$project)) project <- entity$descriptions$project
   
-  #TODO spatialRaster
-  #TODO spatialVector
+  #attributes
+  createAttribute = function(attributeName = character(0),
+                             attributeLabel = character(0),
+                             attributeDefinition = character(0),
+                             measurementScale = character(0),
+                             domain = character(0),
+                             formatString = character(0),
+                             definition = character(0),
+                             unit = character(0),
+                             numberType = character(0),
+                             missingValueCode = character(0),
+                             missingValueCodeExplanation = character(0)){
+    data.frame(
+      attributeName = attributeName,
+      attributeLabel = attributeLabel,
+      attributeDefinition = attributeDefinition,
+      measurementScale = measurementScale,
+      domain = domain,
+      formatString = formatString,
+      definition = definition,
+      unit = unit,
+      numberType = numberType,
+      missingValueCode = missingValueCode,
+      missingValueCodeExplanation = missingValueCodeExplanation,
+      stringsAsFactors = FALSE
+    )
+  }
+    
+  #spatialVector
+  if(!is.null(entity$data)) if(!is.null(entity$data$features)){
+    features = entity$data$features
+    attributeList = createAttribute()
+    columns <- colnames(features)
+    for(featureAttrName in columns){
+      config$logger.info(sprintf("EML: adding '%s' to attributeList", featureAttrName))
+      fat_attr_register <- NULL
+      
+      #create attribute
+      #default name (from data)
+      memberName <- featureAttrName
+      
+      fat_attr <- NULL
+      fat_attr_desc <- NULL
+      fto <- entity$data$featureTypeObj
+      if(!is.null(fto)) fat_attr <- fto$getMemberById(featureAttrName)
+      if(!is.null(fat_attr)){
+        fat_attr_desc <- fat_attr$name
+        registerId <- fat_attr$registerId
+        if(!is.null(registerId)) if(!is.na(registerId)){
+          registers <- config$registers
+          registers <- registers[sapply(registers, function(x){x$id == registerId})]
+          if(length(registers)==0){
+            warnMsg <- sprintf("Unknown register '%s'. Ignored for creating EML attributeList", registerId)
+            config$logger.warn(warnMsg)
+          }else{
+            fat_attr_register <- registers[[1]]
+          }
+        }
+        if(!is.null(fat_attr_desc)) memberName <- fat_attr_desc
+      }
+      
+      
+      #uom
+      uom <- fat_attr$uom
+      if(!is.null(uom)){
+        if(!uom %in% EML::get_unitList()$units$id){
+          config$logger.warn(sprintf("Unit '%s' not referenced in EML::get_unitList()$units IDs", uom))
+          if(!uom %in% EML::get_unitList()$units$abbreviation){
+            config$logger.warn(sprintf("Unit '%s' not referenced in EML::get_unitList()$units Abbreviations", uom))
+          }else{
+            config$logger.warn(sprintf("Unit '%s' found in EML::get_unitList()$units Abbreviations", uom))
+            eml_unit = EML::get_unitList()$units[EML::get_unitList()$units$abbreviation == uom,]
+            config$logger.warn(sprintf("Unit for '%s' attribute, set unit ID '%s' instead of abbreviation", featureAttrName, eml_unit[1L, "id"]))
+            uom <- eml_unit
+          }
+        }
+      }else{
+        if(is(featureAttrValues, "sfc")){
+          uom <- st_crs(features)$units
+          if(is.null(uom)){
+            if(entity$srid == 4326){
+              uom <- "degree"
+            }
+          }
+        }
+      }
+      if(is.null(uom)) uom = NA
+      
+      #add listed values
+      featureAttrValues <- switch(class(features)[1],
+                                  "sf" = features[,featureAttrName][[1]],
+                                  "data.frame" = features[,featureAttrName]
+      )
+      #measurementScale
+      measurementScale <- switch(class(featureAttrValues[1])[1],
+                         "integer" = "ratio",
+                         "numeric" = "ratio",
+                         "character" = "nominal",
+                         "logical" = "ordinal",
+                         "Date" = "dateTime",
+                         "POSIXct" = "dateTime",
+                         "sfc_POINT" = "interval",
+                         "sfc_MULTIPOINT" = "interval",
+                         "sfc_LINESTRING" = "interval",
+                         "sfc_MULTILINESTRING" = "interval",
+                         "sfc_POLYGON" = "interval",
+                         "sfc_MULTIPOLYGON" = "interval"
+      )
+      #domain
+      domain <- switch(class(featureAttrValues[1])[1],
+                       "integer" = "numericDomain",
+                       "numeric" = "numericDomain",
+                       "character" = "textDomain",
+                       "logical" = "enumeratedDomain",
+                       "Date" = "dateTime",
+                       "POSIXct" = "dateTime",
+                       "sfc_POINT" = "numericDomain",
+                       "sfc_MULTIPOINT" = "numericDomain",
+                       "sfc_LINESTRING" = "numericDomain",
+                       "sfc_MULTILINESTRING" = "numericDomain",
+                       "sfc_POLYGON" = "numericDomain",
+                       "sfc_MULTIPOLYGON" = "numericDomain"
+      )
+      if(domain == "textDomain") if(!is.null(fat_attr_register)) if(is(fat_attr_register$data, "data.frame")){
+        domain = "enumeratedDomain"
+      }
+      
+      attribute = createAttribute(
+        attributeName = featureAttrName,
+        attributeLabel = memberName,
+        attributeDefinition = ifelse(is.null(fat_attr_desc), "", fat_attr_desc),
+        measurementScale = measurementScale,
+        domain = domain,
+        formatString = switch(class(featureAttrValues[1])[1],
+          "Date" = "YYYY-MM-DD",
+          "POSIXct" = "YYYY-MM-DD HH:mm:ss",
+          NA
+        ),
+        definition = switch(domain, "textDomain" = ifelse(is.null(fat_attr_desc), NA, fat_attr_desc), NA),
+        unit = switch(domain, "numericDomain" = uom, NA),
+        numberType = switch(domain,
+          "numericDomain" = {
+            switch(class(featureAttrValues[1])[1],
+              "integer" = "integer",
+              "numeric" = "real",
+              "real"
+            )
+          }, NA),
+        missingValueCode = "NA",
+        missingValueCodeExplanation = NA
+      )
+      attributeList <- rbind(attributeList, attribute)
+      
+    }
+      
+    if(is(entity$data$features, "sf")){
+      #use spatialVector
+      config$logger.info("EML: spatial dataset - filling attributeList as 'spatialVector'")
+      dataset$spatialVector = eml$spatialVector(
+        alternateIdentifier = basename(entity$getJobDataResource(config, entity$data$source[[1]])),
+        entityName = entity$title,
+        entityDescription = entity$descriptions[["abstract"]],
+        coverage = dataset$coverage,
+        attributeList = EML::set_attributes(attributes = attributeList),
+        geometry = switch(class(st_geometry(entity$data$features))[1],
+          "sfc_POINT" = "Point",
+          "sfc_MULTIPOINT" = "MultiPoint",
+          "sfc_LINESTRING" = "LineString",
+          "sfc_MULTILINESTRING" = "MultiLineString",
+          "sfc_POLYGON" = "Polygon",
+          "sfc_MULTIPOLYGON" = "MultiPolygon"                
+        ),
+        geometricObjectCount = nrow(entity$data$features),
+        spatialReference = list(horizCoordSysName = {
+          if(!is.null(entity$srid)){
+            paste0("EPSG:", entity$srid)
+          }else{
+            as(sf::st_crs(entity$data$features), "character")[1]
+          }
+        })
+      )
+    }else{
+      #use dataTable
+      config$logger.info("EML: non-spatial dataset - filling attributeList as 'dataTable'")
+      dataset$dataTable = eml$dataTable(
+        alternateIdentifier = basename(entity$getJobDataResource(config, entity$data$source[[1]])),
+        entityName = entity$title,
+        entityDescription = entity$descriptions[["abstract"]],
+        coverage = dataset$coverage,
+        attributeList = EML::set_attributes(attributes = attributeList),
+        numberOfRecords = nrow(entity$data$features)
+      )
+    }
+  }
   
   #wrapper
   eml_md <- EML::eml$eml(
