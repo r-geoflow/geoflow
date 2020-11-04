fc046184419fad760126b90bd4c6784fc8e6bbeb
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2020-11-04 16:16:16 +0100

    debug onLoad

--- R/profile.R
+++ R/profile.R
@@ -3,23 +3,31 @@
   assign(".geoflow", new.env(), envir= asNamespace(pkgname))
   
   #default line separator
+  packageStartupMessage("Setting line separator...")
   set_line_separator()
   
   #defaut temp directory
+  packageStartupMessage("Setting temp directory...")
   set_temp_directory()
   
   #embedded handlers
+  packageStartupMessage("Loading 'contact' handlers...")
   register_contact_handlers()
+  packageStartupMessage("Loading 'entity' handlers...")
   register_entity_handlers()
+  packageStartupMessage("Loading 'dictionary' handlers...")
   register_dictionary_handlers()
   
   #software
+  packageStartupMessage("Loading software...")
   register_software()
   
   #registers
+  packageStartupMessage("Loading registers...")
   register_registers()
   
   #embedded actions
+  packageStartupMessage("Loading actions...")
   register_actions()
   
 } # nocov end
