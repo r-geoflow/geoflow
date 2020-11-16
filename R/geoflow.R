2979fe4eae8e1da455a1d85fa37eccd0ad21ba4b
eblondel <emmanuel.blondel1@gmail.com>
2020-11-16 16:04:19 +0100

    geoflow pkg date

--- R/geoflow.R
+++ R/geoflow.R
@@ -24,8 +24,8 @@
 #'  \tabular{ll}{
 #'    Package: \tab geoflow\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 0.0.20201104\cr
-#'    Date: \tab 2020-11-04\cr
+#'    Version: \tab 0.0.20201116\cr
+#'    Date: \tab 2020-11-16\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
