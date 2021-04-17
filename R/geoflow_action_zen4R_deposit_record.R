56f4134005bb3c64aa23962e8976444e292a4e5a
eblondel <emmanuel.blondel1@gmail.com>
2021-04-17 15:33:46 +0200

    #146

--- R/geoflow_action_zen4R_deposit_record.R
+++ R/geoflow_action_zen4R_deposit_record.R
@@ -212,7 +212,6 @@ zen4R_deposit_record <- function(entity, config, options){
     #upload data files, if any
     data_files <- list.files(file.path(getwd(),"data"))
     if(length(data_files)>0){
-      data_files <- data_files[regexpr(entity$identifiers[["id"]],data_files)>0]
       if(length(data_files)>0) data_files <- data_files[!endsWith(data_files, ".rds")]
       if(length(data_files)>0){
         if(entity$data$upload){
@@ -229,7 +228,6 @@ zen4R_deposit_record <- function(entity, config, options){
     #upload metadata files, if any
     metadata_files <- list.files(file.path(getwd(),"metadata"))
     if(length(metadata_files)>0){
-      metadata_files <- metadata_files[regexpr(entity$identifiers[["id"]],metadata_files)>0]
       if(length(metadata_files)>0) metadata_files <- metadata_files[!endsWith(metadata_files, ".rds")]
       if(length(metadata_files)>0){
         config$logger.info("Zenodo: uploading metadata files...")
