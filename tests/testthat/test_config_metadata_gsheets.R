0646760e594b8b86681c5b8df53e95603f55a9ea
eblondel <emmanuel.blondel1@gmail.com>
2021-10-11 19:49:56 +0200

    #40 set-up tests

---
+++ tests/testthat/test_config_metadata_gsheets.R
@@ -0,0 +1,33 @@
+# test_config_metadata_gsheets.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for config_metadata_gsheets.json workflow
+#=======================
+require(geoflow, quietly = TRUE)
+require(testthat)
+
+cfg_file = system.file("extdata/workflows/config_example_gsheets.json", package = "geoflow")
+
+#init
+test_that("init",{
+  CFG <- geoflow::initWorkflow(cfg_file)
+  expect_is(CFG$metadata$content, "list")
+  expect_equal(length(CFG$metadata$content), 2L)
+  expect_equal(names(CFG$metadata$content), c("contacts", "entities"))
+  expect_equal(length(CFG$metadata$content$contacts), 3L)
+  expect_equal(length(CFG$metadata$content$entities), 2L)
+})
+
+#debug
+test_that("debug",{
+  DEBUG <- geoflow::debugWorkflow(cfg_file)
+  expect_equal(names(DEBUG), c("config", "entity"))
+  expect_is(DEBUG$config, "list")
+  expect_is(DEBUG$entity, "geoflow_entity")
+})
+
+#execute
+test_that("execute",{
+  EXEC <- geoflow::executeWorkflow(cfg_file)
+  expect_true(dir.exists(EXEC))
+})
