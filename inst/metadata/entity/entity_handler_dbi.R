b64a031249c20822f52aff6aa5d371c0ce8cc49d
eblondel <emmanuel.blondel1@gmail.com>
2023-07-21 00:09:41 +0200

    #337 manage overall behavior + base for geometry_columns dbi handler

--- inst/metadata/entity/entity_handler_dbi.R
+++ inst/metadata/entity/entity_handler_dbi.R
@@ -1,12 +1,15 @@
 #handle_entities_dbi
 handle_entities_dbi <- function(config, source, handle = TRUE){
   dbi <- config$software$input$dbi
+  dbi_config <- config$software$input$dbi_config
   if(is.null(dbi)){
     stop("There is no database input software configured to handle entities from DB")
   }
   
   #db source
-  is_query <- startsWith(tolower(source), "select ")
+  is_default_source = FALSE
+  query_all_tables = FALSE
+  is_query <- if(!is.null(source)) startsWith(tolower(source), "select ") else FALSE
   if(is_query){
     source <- try(DBI::dbGetQuery(dbi, source))
     if(is(source,"try-error")){
@@ -15,17 +18,34 @@ handle_entities_dbi <- function(config, source, handle = TRUE){
       stop(errMsg)
     }
   }else{
+    default_metadata_table = if(!is.null(dbi_config$properties$metadata_table)) dbi_config$properties$metadata_table else "public.metadata"
+    if(is.null(source)) source = default_metadata_table; is_default_source = TRUE
+    if(source == "") source = default_metadata_table; is_default_source = TRUE
     source <- try(DBI::dbGetQuery(dbi, sprintf("select * from %s", source)))
     if(is(source,"try-error")){
-      errMsg <- sprintf("Error while trying to read DB table/view '%s'. Check if it exists in DB.", source)
-      config$logger.error(errMsg)
-      stop(errMsg)
+      if(is_default_source){
+        query_all_tables <- TRUE
+        warnMsg <- sprintf("Error while trying to read DB default table '%s' (table not in DB). Switch to generic DBI tables entity handler", source)
+        config$logger.warn(warnMsg)
+      }else{
+        errMsg <- sprintf("Error while trying to read DB table/view '%s'. Check if it exists in DB.", source)
+        config$logger.error(errMsg)
+        stop(errMsg)
+      }
     }
   }
   if(!handle) return(source)
   
   #apply generic handler
-  handle_entities_df <- source(system.file("metadata/entity", "entity_handler_df.R", package = "geoflow"))$value
-  entities <- handle_entities_df(config, source)
+  entities <- if(query_all_tables){
+    #use a generic DBI geometry columns 
+    source = dbi_config$parameters$dbname
+    handle_entities_dbi_geometry_columns <-  source(system.file("metadata/entity", "entity_handler_dbi_geometry_columns.R", package = "geoflow"))$value
+    handle_entities_dbi_geometry_columns(config, source)
+  }else{
+    #use the df entity handler based on the SQL query/table specified as source
+    handle_entities_df <- source(system.file("metadata/entity", "entity_handler_df.R", package = "geoflow"))$value
+    handle_entities_df(config, source)
+  }
   return(entities)
 }
---
+++ inst/metadata/entity/entity_handler_dbi_geometry_columns.R
@@ -0,0 +1,40 @@
+#handle_entities_dbi
+handle_entities_dbi <- function(config, source, handle = TRUE){
+  dbi <- config$software$input$dbi
+  dbi_config <- config$software$input$dbi_config
+  if(is.null(dbi)){
+    stop("There is no database input software configured to handle entities from DB")
+  }
+  
+  db_tables_query = sprintf("select * from geometry_columns where f_table_catalog = '%s'", source)
+  db_tables <- try(DBI::dbGetQuery(dbi, db_tables_query))
+  if(is(db_tables,"try-error")){
+    errMsg <- sprintf("Error while trying to execute DB query '%s'.", db_tables_query)
+    config$logger.error(errMsg)
+    stop(errMsg)
+  }
+  
+  #DB comment utils
+  #getDBTableComment
+  getDBTableComment = function(dbi, schema, table){
+    get_comment_sql = sprintf("select obj_description('%s.%s'::regclass, 'pg_class')",
+                              schema, table)
+    get_comment = DBI::dbGetQuery(dbi, get_comment_sql)
+    return(get_comment$obj_description)
+  }
+  
+  #entities
+  entities = lapply(1:nrow(db_tables), function(i){
+    db_table = db_tables[i,]
+    entity = geoflow_entity$new()
+    id = paste0(source, "_", db_table$f_table_schema,"_", db_table$f_table_name)
+    entity$setIdentifier(key = "id", id)
+    tbl_comment = getDBTableComment(dbi, db_table$f_table_schema, db_table$f_table_name)
+    if(is.na(tbl_comment)) tbl_comment = db_table$f_table_name
+    entity$setTitle(key = "title", tbl_comment)
+    entity$setDescription(key = "abstract", tbl_comment)
+    #TODO to further expand
+    return(entity)
+  })
+  return(entities)
+}
