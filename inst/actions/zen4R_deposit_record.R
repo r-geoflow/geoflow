f5442c07d3ed7cc8712a563f52b6d4f32f30aa71
Sylvain POULAIN <kikislater@users.noreply.github.com>
2023-09-06 08:01:04 +0400

    Update zen4R_deposit_record.R

--- inst/actions/zen4R_deposit_record.R
+++ inst/actions/zen4R_deposit_record.R
@@ -244,7 +244,7 @@ function(action, entity, config){
         zenodo_metadata$setAccessRight(accessRight)
           if ("embargoed" %in% accessRight) {
             config$logger.info(sprintf("Embargoed! Looking for embargoed date..."))
-            embargoDate <- as.Date(entity$rights[sapply(entity$rights, function(x){tolower(x$key) == "embargodate"})][[1]]$values[[1]])
+            embargoDate <- as.Date(entity$date[sapply(entity$date, function(x){tolower(x$key) == "embargo"})][[1]]$values[[1]])
             zenodo_metadata$setEmbargoDate(embargoDate)
             }
           else if ("restricted" %in% accessRight) {
