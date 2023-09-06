7dc1de798d332ecbf94f674bb4ffbf08b9a55278
Sylvain POULAIN <kikislater@users.noreply.github.com>
2023-09-06 16:39:05 +0400

    minor as.Date change

--- inst/actions/zen4R_deposit_record.R
+++ inst/actions/zen4R_deposit_record.R
@@ -247,7 +247,7 @@ function(action, entity, config){
             config$logger.info(sprintf("Embargoed! Looking for embargoed date..."))
             # embargoDate <- as.Date(entity$dates[sapply(entity$dates, function(x){tolower(x$key) == "embargo"})][[1]]$values[[1]])
             embargoDates <- entity$dates[sapply(entity$dates, function(date){date$key == "embargo"})]
-            embargoDate <- if(length(embargoDates)>0) embargoDates[[1]]$values[[1]] else config$logger.error(sprintf("Zenodo: 'embargo' not set in entity$dates whereas 'embargoed' is set in entity$rights. Please check your entities !"))
+            embargoDate <- if(length(embargoDates)>0) as.Date(embargoDates[[1]]$values[[1]]) else config$logger.error(sprintf("Zenodo: 'embargo' not set in entity$dates whereas 'embargoed' is set in entity$rights. Please check your entities !"))
             zenodo_metadata$setEmbargoDate(embargoDate)
             }
           else if ("restricted" %in% accessRight) {
